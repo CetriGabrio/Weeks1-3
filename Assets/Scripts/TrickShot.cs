df027b914b7430d85dd4da7bdb2bbdf967dd2aae
Ilacqua <Ilacqua@OA-S141-12>
2025-01-23 11:02:58 -0500

    Playes around with some different solutions for this coding gym challenge

--- Assets/Scripts/TrickShot.cs
+++ Assets/Scripts/TrickShot.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TrickShot : MonoBehaviour
 {
     [Range(0, 1)]
     public float t;
-    [SerializeField] public float speed = 10;
+    public float speed = 10;
     public AnimationCurve curve;
-
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +23,9 @@ public class TrickShot : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            transform.position += transform.up * speed * Time.deltaTime; //* //curve.Evaluate(t);
+            transform.position += transform.up * speed * Time.deltaTime; //* curve.Evaluate(t);
             t += t * Time.deltaTime;
         }
     }
 }
+
