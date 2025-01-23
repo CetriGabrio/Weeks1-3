098d214afc59f2a55ce197fc89ec53f2953f0d6d
Ilacqua <Ilacqua@OA-S141-12>
2025-01-23 11:18:10 -0500

    Coding Gym 8: Made a prefab and script to spawn capsules with random colors and sizes when clicking

---
+++ Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    public GameObject spritePrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            GameObject newSprite = Instantiate(spritePrefab, mousePosition, Quaternion.identity);
+
+            SpriteRenderer spriteRenderer = newSprite.GetComponent<SpriteRenderer>();
+            spriteRenderer.color = Random.ColorHSV();
+
+            float randomSize = Random.Range(0.5f, 1.0f);
+            newSprite.transform.localScale = new Vector3(randomSize, randomSize, randomSize);
+
+        }
+    }
+}
