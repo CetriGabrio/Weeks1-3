fd111eec78095abee85f42070be1c7824c588476
Ilacqua <Ilacqua@OA-S141-12>
2025-01-23 10:20:49 -0500

    Added ball movement to the right and left, and bouncing on the screen

---
+++ Assets/Scripts/TrickShot.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrickShot : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 0.2f;
+
+    [Range(0, 1)]
+    public float t;
+    public AnimationCurve curve;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        pos.x += speed;
+
+        Vector2 squareInScreenSpace = Camera.main.WorldToScreenPoint(pos);
+
+        if (squareInScreenSpace.x < 0 || squareInScreenSpace.x > Screen.width)
+        {
+            speed = speed * -1;
+        }
+
+        transform.position = pos;
+
+    }
+}
