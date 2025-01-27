b79d29f45c9c0a666a347407bc7ffa99aa1723fc
ilacqua <ilacqua@OA-S138-17>
2025-01-27 12:20:50 -0500

    Created the spaceship and added both a movement and rotation component to command the spaceship with WASD

---
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour
+{
+    [SerializeField] float turnSpeed = 1.0f;
+    [SerializeField] float moveSpeed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float turnAmount = Input.GetAxis("Horizontal") * turnSpeed;
+        transform.Rotate(0, 0, -turnAmount);
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Translate(0, moveAmount, 0);
+    }
+}
