0f534ffeb139723bcf40c4fc1f6d7652de91ad5e
ilacqua <ilacqua@OA-S138-17>
2025-01-27 13:08:36 -0500

    Implemented an animation curve that makes the pulse effect for the star

--- Assets/Scripts/StarPulse.cs
+++ Assets/Scripts/StarPulse.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class StarPulse : MonoBehaviour
 {
+    public AnimationCurve curve;
+
+    [Range(0f,1)]
+    public float t;
+
+    public float speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        t = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        t += Time.deltaTime * speed;
+
+        if(t > 1)
+        {
+            t = 0f;
+        }
+        transform.localScale = Vector2.one * curve.Evaluate(t);
     }
 }
