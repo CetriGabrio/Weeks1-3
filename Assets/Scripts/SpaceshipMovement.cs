914177b24b6f317f819a111a327081962dc5d67c
Ilacqua <Ilacqua@OA-S145-03>
2025-01-29 12:28:46 -0500

    Updated the spaceship behaviour to fix the boundaries and avoid the ship leaving the camera view

--- Assets/Scripts/SpaceshipMovement.cs
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -5,20 +5,34 @@ using static UnityEditor.PlayerSettings;
 
 public class SpaceshipMovement : MonoBehaviour
 {
-    [SerializeField] float turnSpeed = 1.0f;
-    [SerializeField] float moveSpeed = 0.01f;
-    // Start is called before the first frame update
+    public float turnSpeed = 1.0f;
+    public float moveSpeed = 0.01f;
+    public float turnAmount;
+    public float moveAmount;
+    
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float turnAmount = Input.GetAxis("Horizontal") * turnSpeed;
+        turnAmount = Input.GetAxis("Horizontal") * turnSpeed;
         transform.Rotate(0, 0, -turnAmount);
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        moveAmount = Input.GetAxis("Vertical") * moveSpeed;
         transform.Translate(0, moveAmount, 0);
+
+        Vector2 pos = transform.position;
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
+
+        if (screenPos.x < 0) screenPos.x = 0;
+        if (screenPos.x > Screen.width) screenPos.x = Screen.width;
+        if (screenPos.y < 0) screenPos.y = 0;
+        if (screenPos.y > Screen.height) screenPos.y = Screen.height;
+
+        transform.position = (Vector2) Camera.main.ScreenToWorldPoint(screenPos);
+
     }
 }
+
